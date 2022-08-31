b369e867223f602037678e8eb5fe3e3567e5e29d
Kirill Prohoda <prohodakirill@gmail.com>
2022-09-01 00:17:17 +0400

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,14 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package my
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+//    args: Array<String>
+   Main().run()
+}
+
+class Main {
+   private val name: String = "Kirill"
+
+    fun run() {
+      println("hello $name")
+   }
 }
